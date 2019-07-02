ea069540b4149d29385f8f3731b8f73787d0e273
Vlad Chabanenko <just.programmer2226@gmail.com>
2019-07-02 13:32:21 +0300

    Add lever script

---
+++ Darkness/Assets/InternalAssets/Scripts/Entities/Lever.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Lever : MonoBehaviour
+{
+    public GameObject stick;
+    public bool isActive;
+    public float speed;
+
+    private void Update()
+    {
+        if(isActive)
+        {
+            stick.transform.rotation = Quaternion.RotateTowards(stick.transform.rotation, Quaternion.Euler(0, 0, -40), speed * Time.deltaTime);
+        }
+        else
+        {
+            stick.transform.rotation = Quaternion.RotateTowards(stick.transform.rotation, Quaternion.Euler(0, 0, 40), speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Player")
+        {
+            Debug.Log("ОТРАБОТАЛО");
+            LeverInteraction leverInteraction = col.gameObject.GetComponent<LeverInteraction>();
+            leverInteraction.currentLever = this;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            LeverInteraction leverInteraction = col.gameObject.GetComponent<LeverInteraction>();
+            leverInteraction.currentLever = null;
+        }
+    }
+}
---
+++ Darkness/Assets/InternalAssets/Scripts/LeverInteraction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverInteraction : MonoBehaviour
+{
+    public Lever currentLever;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && currentLever != null)
+        {
+            currentLever.isActive = !currentLever.isActive;
+        }
+    }
+}
